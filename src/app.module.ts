b14ea100433b911324a58b65f0ad235d9a431f26
scarlet <scar.andrecoto@gmail.com>
2024-09-08 15:20:59 -0600

    feat: implementacion del modulo de produccion Huevos (se añaden dependencias para dtos, entities y bd)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProduccionHuevosModule } from './produccion-huevos/produccion-huevos.module';
+import { ProduccionHuevo } from './produccion-huevos/entities/produccion-huevo.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: '172.16.60.253', // IP donde se encuentra la base de datos o LOCALHOST (127.0.0.1)
+      port: 1433, // puerto
+      username: 'sa', // usuario de base de datos
+      password: '', // contraseña de base de datos
+      database: 'GranjaAvicolaDB', // nombre de la base de datos
+      entities: [ProduccionHuevo], // Aquí se agregaran todas las entidades de base de datos.
+      logging: false, // se establecio en falso para evitar que devuelva la consulta sql que esta realizando
+      synchronize: false, // se establecio en false para evitar que se sobreescriba y afecte la base de datos, ya que ya esta definida
+      options: {
+        encrypt: false,
+        trustServerCertificate: false,
+      },
+    }),
+    //Despues de TypeOrmModule se agregaran todos los modulos siguientes.
+    ProduccionHuevosModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1'); // establecer un prefijo global a toda la app rest.
   await app.listen(3000);
 }
 bootstrap();
